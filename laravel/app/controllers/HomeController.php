d2789f2cd398ab52f9b519f812fb1f6924a2343d
Luka <redreaper132@gmail.com>
2014-04-12 23:37:27 +0200

    Removed fugly dump

--- laravel/app/controllers/HomeController.php
+++ laravel/app/controllers/HomeController.php
@@ -17,9 +17,9 @@ class HomeController extends BaseController {
 
 	public function showWelcome()
 	{
-		$result = DB::table('test')->get();
-		foreach ($result as $r)
-			var_dump($r);
+		//$result = DB::table('test')->get();
+		//foreach ($result as $r)
+		//	var_dump($r);
 		return View::make('hello');
 	}
 
