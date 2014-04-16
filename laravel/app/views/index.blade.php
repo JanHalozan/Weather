6caf6453440b363bfa3a4a0ac95abf42abfe0126
SM4rk <smdobrota@gmail.com>
2014-04-16 12:02:05 +0200

    Started index page design, functionality

--- laravel/app/views/index.blade.php
+++ laravel/app/views/index.blade.php
@@ -1,25 +1,22 @@
 @extends('layouts.master')
-<!--
-* Created by PhpStorm.
-* User: janhalozan
-* Date: 4/15/14
-* Time: 2:53 PM
-*
-* Index page, built on master template
-*
-*
-*
--->
+
+@section('head')
+	{{HTML::style('css/index.css')}}
+@stop
 
 @section('content')
+	<div id="frame">
+		<div id="weather-guy">
+			
+		</div>
+		
+		<div id="weather-panel">
+			
+		</div>
 
-{{-- This is a blade comment, I will remove it soon, now its here just for the reference --}}
-{{-- Here we echo out the data that we got from our controller --}}
-{{-- $data --}}
-
-<h4>
-    This site is under construction. It will be up and running shortly.
-</h4>
-
+		<div id="tasks">
+			
+		</div>
+	</div>
 @stop
 
