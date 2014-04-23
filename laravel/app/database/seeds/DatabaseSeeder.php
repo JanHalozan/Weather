4903674c2386e2253d9b1baa77311221a408fa5b
Luka <redreaper132@gmail.com>
2014-04-23 10:31:51 +0200

    Removed old seeders

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -10,33 +10,6 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-
-        $this->call('CitiesTableSeeder');
-		$this->call('TestTableSeeder');
 	}
 
-}
-
-class CitiesTableSeeder extends Seeder
-{
-    public function run()
-    {
-        DB::table('cities')->insert(array(
-            array('name' => 'Maribor', 'woeid' => '530779'),
-            array('name' => 'Koper', 'woeid' => '530447'),
-            array('name' => 'Ljubljana', 'woeid' => '530634'),
-            array('name' => 'Ptuj', 'woeid' => '531279')
-        ));
-    }
-}
-
-class TestTableSeeder extends Seeder
-{
-	public function run()
-	{
-		DB::table('test')->insert(array(
-			array('test_text' => 'lep dan', 'working' => 1),
-			array('test_text' => 'ni lep dan', 'working' => 0)
-		));
-	}
 }
