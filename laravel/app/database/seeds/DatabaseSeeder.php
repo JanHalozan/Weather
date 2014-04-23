8340c412b869e9df99e1dfa9100f4b343694d399
Luka <redreaper132@gmail.com>
2014-04-23 12:28:54 +0200

    Added some countries and cities into DB

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -10,6 +10,33 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+        $this->call('CountrySeeder');
+        $this->call('CitiesSeeder');
 	}
 
+}
+
+class CountrySeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('countries')->insert(array(
+            array('name' => 'Slovenia', 'continent' => 'Europe', 'country_code' => 'SI'),
+            array('name' => 'Germany', 'continent' => 'Europe', 'country_code' => 'DE'),
+            array('name' => 'United Kingdom', 'continent' => 'Europe', 'country_code' => 'UK')
+        ));
+    }
+}
+
+class CitiesSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('cities')->insert(array(
+            array('name' => 'Maribor', 'country_id' => DB::table('countries')->where('name', 'Slovenia')->pluck('id'),
+            'latitude' => 46.55, 'longitude' => 15.64, 'api_id' => '3195506'),
+            array('name' => 'Ljubljana', 'country_id' => DB::table('countries')->where('name', 'Slovenia')->pluck('id'),
+                'latitude' => 46.05, 'longitude' => 14.50, 'api_id' => '3196359'),
+        ));
+    }
 }
