b39f948cfbac6c757272cb8a57a3c5ae6f1bf295
Luka <redreaper132@gmail.com>
2014-04-23 16:26:33 +0200

    Added seeder for weather conditions

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -10,8 +10,9 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-        $this->call('CountrySeeder');
-        $this->call('CitiesSeeder');
+        //$this->call('CountrySeeder');
+        //$this->call('CitiesSeeder');
+        $this->call('WeatherConditionSeeder');
 	}
 
 }
@@ -37,6 +38,24 @@ class CitiesSeeder extends Seeder
             'latitude' => 46.55, 'longitude' => 15.64, 'api_id' => '3195506'),
             array('name' => 'Ljubljana', 'country_id' => DB::table('countries')->where('name', 'Slovenia')->pluck('id'),
                 'latitude' => 46.05, 'longitude' => 14.50, 'api_id' => '3196359'),
+        ));
+    }
+}
+
+class WeatherConditionSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('weather_conditions')->insert(array(
+            array('condition' => 'Sky is clear'),
+            array('condition' => 'Few clouds'),
+            array('condition' => 'Scattered clouds'),
+            array('condition' => 'Broken clouds'),
+            array('condition' => 'Shower rain'),
+            array('condition' => 'Rain'),
+            array('condition' => 'Thunderstorm'),
+            array('condition' => 'Snow'),
+            array('condition' => 'Mist'),
         ));
     }
 }
