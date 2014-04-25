42e3a41212dc8d840af9b327b442a9353b5f253f
Majster25 <j.halozan@gmail.com>
2014-04-25 21:46:27 +0200

    Restored "css @media fix, fact parsing fix"

--- laravel/app/controllers/IndexController.php
+++ laravel/app/controllers/IndexController.php
@@ -1,49 +1,2 @@
-<?php
-
-/**
- * Created by PhpStorm.
- * User: janhalozan
- * Date: 4/15/14
- * Time: 2:37 PM
- *
- * Route: /
- * Model: <none - yet>
- *
- *
- */
-class IndexController extends BaseController 
-{
-
-    //This is the default action
-    public function index() 
-    {
-        //Get the user location
-        $place = 'Maribor';
-
-        //Populate our view with data for the current city
-        $view = View::make('index');
-
-        //$record = CurrentWeather::where('name', 'like', $place);
-		$record = CurrentWeather::first()->pluck('temperature');
-		$view->temperature = round($record);
-
-        /* Useless Fact parse */
-        // Get content of html
-        @$html = file_get_contents("http://uselessfacts.net/");
-        
-        // Load that content and create xPath
-        $doc = new DOMDocument();
-        @$doc->loadHTML($html);
-        $xPath = new DomXPath($doc);
-        
-        // Find all facts on the first page and chose one randomly
-       	$temp = $xPath->query("//div[@class='facttext']");
-        $factNumber = rand(0, $temp->length-1);
-
-        // Create variable fact which is passed to the view
-        $view->fact = $temp->item($factNumber)->nodeValue;
-        
-        return $view;
-    }
-
-}
+<?php/** * Created by PhpStorm. * User: janhalozan * Date: 4/15/14 * Time: 2:37 PM * * Route: / * Model: <none - yet> * * */class IndexController extends BaseController {    //This is the default action    public function index()     {        //Get the user location        $place = 'Maribor';        //Populate our view with data for the current city        $view = View::make('index');        //$record = CurrentWeather::where('name', 'like', $place);        $record = CurrentWeather::first()->pluck('temperature');        $view->temperature = round($record);        /* Useless Fact parse */        // Get content of html        @$html = file_get_contents("http://uselessfacts.net/");                if($html === false)
+		{        	$view->fact = "That we encountered an error while parsing your fact.";        }        else        {	        // Load that content and create xPath	        $doc = new DOMDocument();	        @$doc->loadHTML($html);	        $xPath = new DomXPath($doc);	        	        // Find all facts on the first page and chose one randomly	       	$temp = $xPath->query("//div[@class='facttext']");	        $factNumber = rand(0, $temp->length-1);	        // Create variable fact which is passed to the view	        $view->fact = $temp->item($factNumber)->nodeValue;        }        return $view;    }}
