38294b8c322fba4d4380a66f059afd04a1dcb451
SM4rk <smdobrota@gmail.com>
2014-06-04 18:59:09 +0200

    What is going on ?

--- laravel/app/controllers/TasksController.php
+++ laravel/app/controllers/TasksController.php
@@ -5,18 +5,72 @@ class TasksController extends BaseController
     {
         $view = View::make('tasks');
 
+        $view->tasks = Activities::all();
+
         return $view;
     }
 
     function addTask()
     {
-    	$task = "0";
 
         if (Request::ajax())
         {
-            $task = Input::get('task');
-        }
-		
-		return $task;    
+            
+            $taskType = Input::get('taskType');
+
+            $treeController = new TreeController();
+            $treeController->loadTrees();
+            $forecast = ForecastWeather::take(5)->get();
+            
+            // For each day
+            for($i = 0; $i < count($forecast); $i++)
+            {
+                // Get output from tree
+                $weatherInfo = $forecast[$i];
+                $reading = $treeController->transformReading($weatherInfo);
+                $output = $treeController->classifyTasks($reading);
+
+                // Preset posibility on 0 in case day dont have that taskType
+                $posibility[$i] = 0;
+
+                // For each element of output
+                for($j = 0; $j < count($output); $j++)
+                {
+                    // If task type is same as picked
+                    if($output[$j][0] === $taskType)
+                    {
+                        // Save posibility of that task
+                        $posibility[$i] = $output[$j][1];
+                        break;
+                    }
+                }
+            }
+            
+            // Find index of the bigest posibility
+            $maxPosibilityVal = 0;
+            // $day = $index
+            $day = -1;
+
+            // Find index of the bigest posibility
+            for($i = 0; $i < count($posibility); $i++)
+            {
+                if($posibility[$i] > $maxPosibilityVal)
+                {
+                    // If day allready have 5 tasks its ignored
+                    if(Input::get("c".$i."Size") < '5'){
+                        $maxPosibilityVal = $posibility[$i];
+                        $day = $i;
+                    }
+                }
+            }
+
+            // If none of the days have that type, we dont recomand it
+            if($maxPosibilityVal == 0)
+            {
+                // Container 6 is trash
+                $day = 5;
+            }     
+        }    
+		return $day;    
     }
 }
