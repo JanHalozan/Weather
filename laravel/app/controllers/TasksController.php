8046bbed2abcc44361fb061772124a4dd718255c
SM4rk <smdobrota@gmail.com>
2014-06-05 10:18:11 +0200

    Task picker forecast query fix, cookie fix

--- laravel/app/controllers/TasksController.php
+++ laravel/app/controllers/TasksController.php
@@ -15,62 +15,69 @@ class TasksController extends BaseController
 
         if (Request::ajax())
         {
-            
-            $taskType = Input::get('taskType');
-
-            $treeController = new TreeController();
-            $treeController->loadTrees();
-            $forecast = ForecastWeather::take(5)->get();
-            
-            // For each day
-            for($i = 0; $i < count($forecast); $i++)
+            if(Cookie::get('city_id') != "")
             {
-                // Get output from tree
-                $weatherInfo = $forecast[$i];
-                $reading = $treeController->transformReading($weatherInfo);
-                $output = $treeController->classifyTasks($reading);
+                $taskType = Input::get('taskType');
 
-                // Preset posibility on 0 in case day dont have that taskType
-                $posibility[$i] = 0;
-
-                // For each element of output
-                for($j = 0; $j < count($output); $j++)
+                $treeController = new TreeController();
+                $treeController->loadTrees();
+                $forecast = ForecastWeather::where('city_id', Cookie::get('city_id'))->orderBy('forecast_date')->take(5)->get();
+                           
+                // For each day
+                for($i = 0; $i < count($forecast); $i++)
                 {
-                    // If task type is same as picked
-                    if($output[$j][0] === $taskType)
+                    // Get output from tree
+                    $weatherInfo = $forecast[$i];
+                    $reading = $treeController->transformReading($weatherInfo);
+                    $output = $treeController->classifyTasks($reading);
+
+                    // Preset posibility on 0 in case day dont have that taskType
+                    $posibility[$i] = 0;
+
+                    // For each element of output
+                    for($j = 0; $j < count($output); $j++)
                     {
-                        // Save posibility of that task
-                        $posibility[$i] = $output[$j][1];
-                        break;
+                        // If task type is same as picked
+                        if($output[$j][0] === $taskType)
+                        {
+                            // Save posibility of that task
+                            $posibility[$i] = $output[$j][1];
+                            break;
+                        }
                     }
                 }
-            }
-            
-            // Find index of the bigest posibility
-            $maxPosibilityVal = 0;
-            // $day = $index
-            $day = -1;
+                
+                // Find index of the bigest posibility
+                $maxPosibilityVal = 0;
+                // $day = $index
+                $day = -1;
 
-            // Find index of the bigest posibility
-            for($i = 0; $i < count($posibility); $i++)
-            {
-                if($posibility[$i] > $maxPosibilityVal)
+                // Find index of the bigest posibility
+                for($i = 0; $i < count($posibility); $i++)
                 {
-                    // If day allready have 5 tasks its ignored
-                    if(Input::get("c".$i."Size") < '5'){
-                        $maxPosibilityVal = $posibility[$i];
-                        $day = $i;
+                    if($posibility[$i] > $maxPosibilityVal)
+                    {
+                        // If day allready have 5 tasks its ignored
+                        if(Input::get("c".$i."Size") < '5'){
+                            $maxPosibilityVal = $posibility[$i];
+                            $day = $i;
+                        }
                     }
                 }
-            }
 
-            // If none of the days have that type, we dont recomand it
-            if($maxPosibilityVal == 0)
+                // If none of the days have that type, we dont recomand it
+                if($maxPosibilityVal == 0)
+                {
+                    // Container 6 is trash
+                    $day = 5;
+                }
+            } 
+            else 
             {
-                // Container 6 is trash
-                $day = 5;
-            }     
+                $day = "No city";
+            }             
         }    
+
 		return $day;    
     }
 }
