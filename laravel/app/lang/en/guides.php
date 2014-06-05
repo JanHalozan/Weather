27ed19a84aa33074f212d41f4b1814fd143d4982
SM4rk <smdobrota@gmail.com>
2014-06-05 16:28:22 +0200

    Index navigation without icons

--- laravel/app/lang/en/guides.php
+++ laravel/app/lang/en/guides.php
@@ -5,6 +5,10 @@ return array(
     'login' => 'Log in',
     'register' => 'Sign up',
     'logout' => 'Log out',
+    'options' => 'Options',
+    'access' => 'Access',
+    'home' => 'Home',
+    'forecast' => 'Forecast',
 
     'clothes_error' => 'There was a problem suiting you up. Using fallback clothes.',
     'forecast_error' => 'No city was detected, please try again.'
--- laravel/app/lang/si/guides.php
+++ laravel/app/lang/si/guides.php
@@ -5,6 +5,10 @@ return array(
     'login' => 'Prijava',
     'signup' => 'Registracija',
     'logout' => 'Odjava',
+    'options' => 'Nastavitve',
+    'access' => 'Dostop',
+    'home' => 'Domov',
+    'forecast' => 'Napoved',
 
     'clothes_error' => 'Težava pri oblačenju, uporabljam privzeta oblačila.',
     'forecast_error' => 'Zaznali nismo nobenega mesta, prosim poskusite ponovno.'
