887b8d12afcda18bd7fdb028b414e4ef42eeb7cb
Majster25 <j.halozan@gmail.com>
2014-06-05 20:31:17 +0200

    Cities query fix

--- laravel/app/controllers/ForecastController.php
+++ laravel/app/controllers/ForecastController.php
@@ -11,14 +11,14 @@ class ForecastController extends BaseController
 		$view = View::make('forecast');
 
 		//Query the forecast for the city in cookie
-		$forecasts = ForecastWeather::where('id', Cookie::get('city_id'))->orderBy('forecast_date')->get();
+		$forecasts = ForecastWeather::where('city_id', Cookie::get('city_id'))->orderBy('forecast_date')->get();
 
 		$days = array();
 
 		foreach ($forecasts as $forecast)
 			array_push($days, array('high' => $forecast->temperature_high, 'low' => $forecast->temperature_low, 'temperature' => $forecast->temperature));
 
-		$tasks = array(array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'));
+		$tasks = array(array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'));
 
 		for ($i = 0; $i < min(count($tasks), count($forecasts)); $i++)
 		{
@@ -27,11 +27,7 @@ class ForecastController extends BaseController
 			$days[$i]['task3'] = $tasks[$i]['task3'];
 		}
 
-		var_dump($days);
-
 		$view->days = $days;
-
-		//$view->days = array(array("temperature" => 20, "high" => 22, "low" => 15, "task1" => "Fuck yourself", "task2" => "HEHE", "task3" => "Burek"), array("temperature" => 20, "high" => 22, "low" => 15, "task1" => "Fuck yourself", "task2" => "HEHE", "task3" => "Burek"), array("temperature" => 20, "high" => 22, "low" => 15, "task1" => "Fuck yourself", "task2" => "HEHE", "task3" => "Burek"), array("temperature" => 20, "high" => 22, "low" => 15, "task1" => "Fuck yourself", "task2" => "HEHE", "task3" => "Burek"), array("temperature" => 20, "high" => 22, "low" => 15, "task1" => "Fuck yourself", "task2" => "HEHE", "task3" => "Burek"));
 
 		return $view;
 	}
