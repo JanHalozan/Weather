326677a787b4838fbfb1f222a67371eea4bda498
SM4rk <smdobrota@gmail.com>
2014-06-05 23:58:54 +0200

    Forcast logic is done

--- laravel/app/controllers/ForecastController.php
+++ laravel/app/controllers/ForecastController.php
@@ -1,5 +1,16 @@
 <?php
 
+function getTask($type)
+{
+    // Get all tasks with activiti that has requiered posibility
+    $tmp = Activities::where('activity_type', $type)->get();
+    // Pick one task randomli among those chosen tasks
+    $max = Activities::where('activity_type', $type)->where('user_id', -1)->count();
+    $randTask = rand(0, $max - 1);
+
+    return $tmp[$randTask]->name;
+}
+
 class ForecastController extends BaseController 
 {
 	public function index()
@@ -17,12 +28,57 @@ class ForecastController extends BaseController
 
 		for ($i = 0; $i < count($forecasts); $i++) 
 		{
+
+			// ----------------- T INFO --------------------
 			array_push($days, array('high' => $forecasts[$i]->temperature_high, 'low' => $forecasts[$i]->temperature_low, 'temperature' => $forecasts[$i]->temperature));
-		
+
+			// ------------------ ICON ---------------------
+
+			//Get the condition string
+	        $condition = Conditions::find($forecasts[$i]->condition_id)->condition;
+
+	        // Decide which icon is picked
+	        switch ($condition) {
+	            case 'clear_sky':
+	                $icon = 'Sun.png';
+	                break;
+	            case 'few_clouds':
+	                $icon = 'MostlyCloudy.png';
+	                break;
+	            case 'scattered_clouds':
+	                $icon = 'MostlyCloudy.png';
+	                break;
+	            case 'broken_clouds':
+	                $icon = 'Cloud.png';
+	                break;
+	            case 'shower_rain':
+	                $icon = 'Rain.png';
+	                break;
+	            case 'rain':
+	                $icon = 'Rain.png';
+	                break;
+	            case 'thunderstorm':
+	                $icon = 'Tunder.png';
+	                break;
+	            case 'snow':
+	                $icon = 'Snow.png';
+	                break;
+	            case 'mist':
+	                $icon = 'Fogg.png';
+	                break;
+	            default:
+	                $icon = 'Cloud.png';
+	                break;
+	        }
+
+	        $days[$i]['icon'] = $icon;
+
+	        //----------------- WeatherGuy -----------------------
+
 			//Get the decision tree and process the needed clothes
 	        $treeController = new TreeController();
 	        $treeController->loadTrees();
-	        $reading = $treeController->transformReading($forecast[$i]);
+	        $reading = $treeController->transformReading($forecasts[$i]);
 	        $output = $treeController->classifyReading($reading);
 
 	        //We could not figure out something, fallback
@@ -40,16 +96,68 @@ class ForecastController extends BaseController
 	            $view->body = $output[1];
 	            $view->pants = $output[2];
 	            $view->boots = $output[3];
-	        }
-		}
+	        }	        
 
-		$tasks = array(array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'), array('task1' => 'Kupi mleko', 'task2' => 'Kupi sir', 'task3' => 'Kupi jogurt'));
 
-		for ($i = 0; $i < min(count($tasks), count($forecasts)); $i++)
-		{
-			$days[$i]['task1'] = $tasks[$i]['task1'];
-			$days[$i]['task2'] = $tasks[$i]['task2'];
-			$days[$i]['task3'] = $tasks[$i]['task3'];
+	        // ------------------ TASKS ----------------------
+	        try{
+
+		        $outputTasks = $treeController->classifyTasks($reading);
+
+		        //Deffine posibilities
+	            $posibility = array(
+	                $outputTasks[0][1],
+	                $outputTasks[0][1]+$outputTasks[1][1],
+	                $outputTasks[0][1]+$outputTasks[1][1]+$outputTasks[2][1]
+	            );
+
+	            // Number of tasks on index page
+	            $numberOfTasks = 3;
+
+	            // For each task
+	            for($j = 0; $j < $numberOfTasks; $j++){
+
+	                // Generate random value
+	                $rand = rand(0,100);
+	                $rand = $rand / 100;
+
+	                // Check which type was chosen and get out task
+	                if($rand <= $posibility[0]){
+	                    $tasks[$j] = getTask($outputTasks[0][0]);
+	                }
+	                else if($rand <= $posibility[1])
+	                {
+	                    $tasks[$j] = getTask($outputTasks[1][0]);
+	                }
+	                else
+	                {
+	                    $tasks[$j] = getTask($outputTasks[2][0]);
+	                }
+
+	                
+	                // Prevents picking same task
+	                for($k = 0; $k < $j; $k++)
+	                {
+	                    if($tasks[$j] == $tasks[$k])
+	                    {
+	                        $j--;
+	                    }
+	                }
+	                
+	            }
+
+	            $days[$i]['task1'] = $tasks[0];
+	            $days[$i]['task2'] = $tasks[1];
+	            $days[$i]['task3'] = $tasks[2]; 
+
+        	}
+			catch (Exception $e)
+	        {
+	            $days[$i]['task1'] = "Error";
+	            $days[$i]['task2'] = "Error";
+	            $days[$i]['task3'] = "Error";
+	        }  
+	        
 		}
 
 		$view->days = $days;
--- laravel/app/views/forecast.blade.php
+++ laravel/app/views/forecast.blade.php
@@ -11,34 +11,35 @@
 			<div class="forecast-frame">
 
 				<div class="weather">
-					<img class="weather-icon" src="/images/Sun.png">
+					<img class="weather-icon" src="images/{{$day['icon']}}">
 					<div class="weather-info">
 						<h2>{{ round($day['temperature']) }}°C</h2>
 						<span>Hi: {{ round($day['high']) }}°C</span><br>
 						<span>Lo: {{ round($day['low']) }}°C</span>
 					</div>
-						<div style="clear: both;"></div>
+					<div style="clear: both;"></div>
+				</div>
+
+				<div style="clear: both;"></div>
+				<div id="weather-guy">			
+					<div id="pants">
+						<img src="images/{{ $pants . '_legs.png' }}"/>
 					</div>
-					
-					<div id="weather-guy">			
-						<div id="pants">
-							<img src="images/{{ $pants . '_legs.png' }}"/>
-						</div>
 
-						<div id="body">
-							<img src="images/{{ $body . '_torso.png' }}"/>
-						</div>
-
-						<div id="head">
-							<img src="images/{{ $head . '_head.png' }}"/>
-						</div>
-
-						<div id="boots">
-							<img src="images/{{ $boots . '_boots.png' }}"/>
-						</div>
-
-						<img src="images/WeatherGuy.png" alt="Weatherguy"/>
+					<div id="body">
+						<img src="images/{{ $body . '_torso.png' }}"/>
 					</div>
+
+					<div id="head">
+						<img src="images/{{ $head . '_head.png' }}"/>
+					</div>
+
+					<div id="boots">
+						<img src="images/{{ $boots . '_boots.png' }}"/>
+					</div>
+
+					<img src="images/WeatherGuy.png" alt="Weatherguy"/>
+				</div>
 				{{-- Only if we're logged in we show the tasks --}}
 				@if(Auth::check())
 				<div class="tasks">
