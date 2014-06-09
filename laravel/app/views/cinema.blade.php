ee26e46f992066998aad3d165c5988f0f68aa33f
Majster25 <j.halozan@gmail.com>
2014-06-09 16:20:17 +0200

    cinema fixes

--- laravel/app/views/cinema.blade.php
+++ laravel/app/views/cinema.blade.php
@@ -1,21 +1,39 @@
 @extends('layouts.master')
 
+@section('head')
+
+{{ HTML::style('css/cinema.css') }}
+
+@stop
+
 @section('content')
 
 <table>
+	<tr>
+		<th>
+			<p>Cover</p>
+		</th>
+		<th>
+			<p>Kratek opis</p>
+		</th>
+	</tr>
 	@foreach($schedule as $item)
 	<tr>
-		<a href="{{ $item['link'] }}" target="_blank">
 	    	<td>
 	    		<img src="{{ $item['image'] }}">
 	    	</td>
 	    	<td>
 	    		<div>
 	    			<h4>{{ $item['name'] }}</h4>
-	    			<p>{{ $item['description'] }}</p>
+
+	    			@if(strlen($item['description']) > 150)
+	    				<p>{{ substr($item['description'], 0, 150) }}...</p>
+	    			@else
+	    				<p>{{ $item['description'] }}</p>
+	    			@endif
 	    		</div>
+	    		<a href="{{ $item['link'] }}" target="_blank"><p class="link">Ogled</p></a>
 	    	</td>
-		</a>
 	</tr>
 	@endforeach
 </table>
