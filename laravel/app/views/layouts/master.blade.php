f56159f3cd17a66369b372a16d66d39f01f3b9c8
Majster25 <j.halozan@gmail.com>
2014-06-09 18:31:25 +0200

    Integrated google analytics

--- laravel/app/views/layouts/master.blade.php
+++ laravel/app/views/layouts/master.blade.php
@@ -2,6 +2,18 @@
 <html>
     <head>
         <meta charset="utf-8">
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-27826743-3', 'weatherbound.net');
+        ga('send', 'pageview');
+
+        </script>
+
         {{ HTML::style('//fonts.googleapis.com/css?family=Roboto:400,100,300,500&subset=latin,latin-ext') }}
         {{ HTML::style('css/master.css') }}
         {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js') }}
