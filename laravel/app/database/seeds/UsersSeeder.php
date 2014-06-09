c5c86bfe032ae406ee9b56947a362e0fa3fc4069
Majster25 <j.halozan@gmail.com>
2014-06-09 19:39:51 +0200

    Changed users seeder

--- laravel/app/database/seeds/UsersSeeder.php
+++ laravel/app/database/seeds/UsersSeeder.php
@@ -5,7 +5,11 @@ class UsersSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert(array(
-            array('username' => 'test', 'password' => Hash::make('geslo'), 'activated' => 1, 'locale' => 'us', 'is_admin' => true)
+        	array('username' => 'luka', 'password' => Hash::make('geslo'), 'activated' => 1, 'locale' => 'us', 'is_admin' => true),
+        	array('username' => 'jan', 'password' => Hash::make('geslo'), 'activated' => 1, 'locale' => 'us', 'is_admin' => true),
+        	array('username' => 'saso', 'password' => Hash::make('geslo'), 'activated' => 1, 'locale' => 'us', 'is_admin' => true),
+        	array('username' => 'zoran', 'password' => Hash::make('geslo'), 'activated' => 1, 'locale' => 'us', 'is_admin' => true),
+        	array('username' => 'martin', 'password' => Hash::make('geslo'), 'activated' => 1, 'locale' => 'us', 'is_admin' => true)
         ));
     }
 }
