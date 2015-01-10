ddb67148b41149f976eab9f0c3267bbe82aba4b1
Luka <redreaper132@gmail.com>
2015-01-10 18:24:34 +0100

    Using some real data for text

--- laravel/app/controllers/GraphicsController.php
+++ laravel/app/controllers/GraphicsController.php
@@ -12,8 +12,15 @@ class GraphicsController extends BaseController
         $city->id = 2;
         $weatherInfo = CurrentWeather::where('city_id', '=', $city->id)->orderBy('reading_time', 'desc')->firstOrFail();
 
+        //Get the condition string
+        $condition = Conditions::find($weatherInfo->condition_id)->condition;
+
+        //Get country data
+        $country = Countries::find($city->country_id);
+
         //Generate JS data blob for HUD usage
-        $data = array('temperature' => $weatherInfo->temperature, 'city_name' => $city->name);
+        $data = array('temperature' => $weatherInfo->temperature, 'city_name' => $city->name, 'condition' => Lang::get('conditions.' . $condition),
+            'country' => $country->name);
         $view->data_blob = json_encode($data);
 
     	return $view;
--- laravel/public/js/graphics/luka_main.js
+++ laravel/public/js/graphics/luka_main.js
@@ -77,8 +77,9 @@ function luka_init()
 	scene.add(grass_mesh);
 
 	//Add some elements
-	text_elements.push(new TextElement("Maribor", 1, 1, -1));
-	text_elements.push(new TextElement("20°C", 1, 0.8, -1));
+	text_elements.push(new TextElement(data_blob.city_name + ', ' + data_blob.country, 1, 1.2, -1));
+	text_elements.push(new TextElement(data_blob.condition, 1, 1, -1));
+	text_elements.push(new TextElement(data_blob.temperature.toFixed(0) + '°C', 1, 0.8, -1));
 	text_elements.push(new TextElement("Much text, so fancy", -1.5, 1, -1));
 	text_elements.push(new TextElement("FERIFax™", -1.5, 1.3, -1));
 
