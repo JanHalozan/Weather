9a5639f910ed693dc24d2afa57e9e0e68a06df76
Sašo <smdobrota@gmail.com>
2015-01-10 20:25:01 +0100

    Added togle button (9), increase / decrease buttons (m,n)

--- laravel/public/js/graphics/saso_main.js
+++ laravel/public/js/graphics/saso_main.js
@@ -12,44 +12,34 @@ function initSnezinke(stSnezink, minX, maxX, minY, maxY, minZ, maxZ) {
 	var snezinkaTexture = THREE.ImageUtils.loadTexture("images/textures/snezinka.png");
 	var snezinkaGeometry = new THREE.PlaneGeometry(velikostSnezinke, velikostSnezinke);
 
+
+	var snezinkaMaterial = [];
+
 	// Materiali različnih prosojnosti za snežinke
-	var snezinkaMaterial1 = new THREE.MeshBasicMaterial({
-		map:snezinkaTexture, 
-		transparent:true, 
-		opacity:0.2});
-
-	var snezinkaMaterial2 = new THREE.MeshBasicMaterial({
-		map:snezinkaTexture, 
-		transparent:true, 
-		opacity:0.4});
-
-	var snezinkaMaterial3 = new THREE.MeshBasicMaterial({
-		map:snezinkaTexture,
-		transparent:true, 
-	 	opacity:0.6});
-
-	var snezinkaMaterial4 = new THREE.MeshBasicMaterial({
-		map:snezinkaTexture,
-		transparent:true, 
-	 	opacity:0.8});
+	for(var i = 1; i < 5; i++) {
+		snezinkaMaterial[i-1] = new THREE.MeshBasicMaterial({
+			map:snezinkaTexture, 
+			transparent:true, 
+			opacity:(0.2*i)});
+	}
 	
 	for(var i = 0; i < stSnezink; i++){
 		// Različne snežinke
 		if(i % 4 == 0)
-			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial1 );
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial[0] );
 		else if(i % 4 == 1)
-			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial2 );
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial[1] );
 		else if(i % 4 == 2)
-			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial3 );
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial[2] );
 		else if(i % 4 == 3)
-			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial4 );	
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial[3] );	
 		
 		snezinke[i].position.x = getRandom(minX, maxX);
 		snezinke[i].position.y = getRandom(minY, maxY);
 		snezinke[i].position.z = getRandom(minZ, maxZ);
-
-		scene.add(snezinke[i]);
 	}	
+
+	scene.fog = new THREE.FogExp2(0x5C97BF, 0.12);
 }
 
 function padanjeSnezink(stSnezink, hitrostPadanja, mocVetra, minY, maxY, minX, maxX) {
@@ -79,8 +69,9 @@ function padanjeSnezink(stSnezink, hitrostPadanja, mocVetra, minY, maxY, minX, m
 	}
 }
 
-var snezinke = []; 
-var stSnezink = 1350;
+var snezinke = [];
+var maxStSnezink = 3000; 
+var stSnezink = 900;
 var velikostSnezinke = 0.13;
 var minX = -15, maxX = 15;
 var minY = 0, maxY = 10;
@@ -88,16 +79,62 @@ var minZ = -12, maxZ = -1;
 var hitrostPadanja = 0.03;
 var mocVetra = 0.005;
 
+var zastavica = 0;
+var isKeyPresed = 0;
+
 /* 
  - GLOBALNE SPREMENLJIVKE IN FUNKCIJE
 */
 
 function saso_init() 
 {
-	initSnezinke(stSnezink, minX, maxX, minY, maxY, minZ, maxZ);
+	initSnezinke(maxStSnezink, minX, maxX, minY, maxY, minZ, maxZ);
+	scene.fog.density = 0;
 }
 
 function saso_update()
 {
-	padanjeSnezink(stSnezink, hitrostPadanja, mocVetra, minY, maxY, minX, maxX);
+	if (keyboard.pressed('9')) {
+		if(isKeyPresed == 0) {
+			if(zastavica == 0) {
+				zastavica = 1;
+				scene.fog.density = 0.12;
+				for(var i = 0; i < stSnezink; i++)
+					scene.add(snezinke[i]);
+			}
+			else {
+				zastavica = 0;
+				scene.fog.density = 0;
+				for(var i = 0; i < stSnezink; i++)
+					scene.remove(snezinke[i]);
+			}
+		}
+		isKeyPresed = 1;
+	} 
+	else if (keyboard.pressed('m')) {
+		if(isKeyPresed == 0) {
+			if(stSnezink < 2900)
+				stSnezink += 100;
+			for(var i = 0; i < 100; i++)
+				scene.add(snezinke[i + (stSnezink - 100)]);
+		}
+		isKeyPresed = 1;
+	}
+	else if (keyboard.pressed('n')) {
+		if(isKeyPresed == 0) {
+			if(stSnezink > 200)
+				stSnezink -= 100;
+			for(var i = 0; i < 100; i++)
+				scene.remove(snezinke[i+stSnezink]);
+		}
+		isKeyPresed = 1;
+	}  
+	else {
+		isKeyPresed = 0;
+	}
+
+
+
+	if(zastavica == 1)
+		padanjeSnezink(stSnezink, hitrostPadanja, mocVetra, minY, maxY, minX, maxX);
 }
