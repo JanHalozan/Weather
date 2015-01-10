4e57411a9f057008ed0ad573159bfea3856658ff
Sašo <smdobrota@gmail.com>
2015-01-10 16:51:11 +0100

    "Spadno je sneg na breg :D" Not finished jet

--- laravel/public/js/graphics/saso_main.js
+++ laravel/public/js/graphics/saso_main.js
@@ -10,48 +10,57 @@ function getRandom(min, max) {
 }
 
 function initSnezinke(stSnezink) {
-
 	// Textura in geometry za snežinko
 	var snezinkaTexture = THREE.ImageUtils.loadTexture("images/textures/snezinka.png");
 	var snezinkaGeometry = new THREE.PlaneGeometry( 0.4, 0.4);
 
 	// Materiali različnih prosojnosti za snežinke
-	var snezinkaMaterial05 = new THREE.MeshLambertMaterial({
+	var snezinkaMaterial05 = new THREE.MeshBasicMaterial({
 		map:snezinkaTexture, 
 		transparent:true, 
 		opacity:0.1});
 
-	var snezinkaMaterial06 = new THREE.MeshLambertMaterial({
+	var snezinkaMaterial06 = new THREE.MeshBasicMaterial({
 		map:snezinkaTexture, 
 		transparent:true, 
 		opacity:0.3});
 
-	var snezinkaMaterial07 = new THREE.MeshLambertMaterial({
+	var snezinkaMaterial07 = new THREE.MeshBasicMaterial({
 		map:snezinkaTexture,
 		transparent:true, 
 	 	opacity:0.5});
 
-	var snezinkaMaterial08 = new THREE.MeshLambertMaterial({
+	var snezinkaMaterial08 = new THREE.MeshBasicMaterial({
 		map:snezinkaTexture,
 		transparent:true, 
 	 	opacity:0.7});
 	
-	// Različne snežinke
-	snezinke[0] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial05 );
-	snezinke[1] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial06 );
-	snezinke[2] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial07 );
-	snezinke[3] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial08 );
+	for(var i = 0; i < stSnezink; i++){
+		// Različne snežinke
+		if(i % 4 == 0)
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial08 );
+		else if(i % 4 == 1)
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial06 );
+		else if(i % 4 == 2)
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial07 );
+		else if(i % 4 == 3)
+			snezinke[i] = new THREE.Mesh( snezinkaGeometry, snezinkaMaterial05 );	
+		
+		snezinke[i].position.x = getRandom(-15,15);
+		snezinke[i].position.z = getRandom(-10,-15);
+		snezinke[i].position.y = getRandom(-15,15);
 
-	snezinke[0].position.x = getRandom(-10,10);
-	snezinke[1].position.x = getRandom(-10,10);
-	snezinke[2].position.x = getRandom(-10,10);
-	snezinke[3].position.x = getRandom(-10,10);
+		scene.add(snezinke[i]);
+	}	
+}
 
-	scene.add(snezinke[0]);
-	scene.add(snezinke[1]);
-	scene.add(snezinke[2]);
-	scene.add(snezinke[3]);
-	
+function padanjeSnezink(stSnezink) {
+	for(var i = 0; i < stSnezink; i++){			
+		snezinke[i].position.x += getRandom(-0.01,0.01);
+		snezinke[i].position.y -= 0.05;
+		if(snezinke[i].position.y < -15.0)
+			snezinke[i].position.y = 15;
+	}
 }
 
 /* 
@@ -60,10 +69,10 @@ function initSnezinke(stSnezink) {
 
 function saso_init() 
 {
-	initSnezinke();
+	initSnezinke(500);
 }
 
 function saso_update()
 {
-
+	padanjeSnezink(500);
 }
