cd52d85dc4ed5962db637f373eedf527c8d55a02
Majster25 <j.halozan@gmail.com>
2015-01-14 21:22:10 +0100

    Tree test

--- laravel/public/js/graphics/jan_main.js
+++ laravel/public/js/graphics/jan_main.js
@@ -1,6 +1,32 @@
+
+
+var rockMesh;
+var treeMesh;
+var houseMesh;
+
 function jan_init() 
 {
-	
+	var loader = new THREE.ObjectLoader();
+
+	loader.load("models/tree.obj", function(object, materials){
+
+		var texture = THREE.ImageUtils.loadTexture("images/textures/tree_tex.png");
+		texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+		texture.repeat.set(50, 50);
+
+		var material = new THREE.MeshLambertMaterial({map: texture, side: THREE.DoubleSide});
+
+		object.traverse(function(child){
+			if (child instanceof THREE.Mesh)
+				child.material = material;
+		});
+
+		object.position.x = 5;
+		object.position.y = 0;
+		object.position.z = 5;
+
+		scene.add(object);
+	});
 }
 
 function jan_update()
